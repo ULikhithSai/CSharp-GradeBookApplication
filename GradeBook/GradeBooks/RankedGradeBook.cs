d5fecd3eec1d5257465c01178029e97eade3350f
ULikhithSai <44312759+ULikhithSai@users.noreply.github.com>
2018-10-20 23:05:51 +0530

    Update GradeBook/GradeBooks/RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -41,6 +41,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             else
             {
@@ -53,6 +54,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             else
             {
